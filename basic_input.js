9b32ca606cf482249a158221045e9517972debf8
Louis <louis@louishoughton.com>
2016-01-17 17:42:36 +0000

    Made text better

--- basic_input.js
+++ basic_input.js
@@ -4,7 +4,13 @@ var gpio = require('rpi-gpio'),
 gpio.setMode(gpio.MODE_BCM);
 
 gpio.on('change', function(channel, value) {
-	console.log('Channel ' + channel + ' value is now ' + value);
+    if (value) {
+        console.log("It's in the house!");
+    }
+    else {
+        console.log("It's gone... for now.");
+    }
+
 });
 
 gpio.setup(pin, gpio.DIR_IN, gpio.EDGE_BOTH, function(err) {
